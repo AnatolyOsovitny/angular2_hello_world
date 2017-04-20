cfb8a9cc26d1a112244a2ef50870cad5f8c6628b
Anatoly Osovitny <anatoly@osovitny.com>
2017-04-21 00:03:59 +0700

    Add project files.

---
+++ angular2_hello_world/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace angular2_hello_world.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
